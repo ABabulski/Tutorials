147eaca33e6a72f4e10b4c676b9faf2298005e93
Andrey Babulski <andreybabulski@gmail.com>
2026-01-10 20:25:40 +0000

    Initial commit

---
+++ FirstProject/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter side A");
+double sideA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter side B");
+double sideB = Convert.ToDouble(Console.ReadLine());
+
+double sideC = Math.Sqrt(Math.Pow(sideA, 2) + Math.Pow(sideB, 2));
+
+Console.WriteLine("Side C equals: " + sideC);
